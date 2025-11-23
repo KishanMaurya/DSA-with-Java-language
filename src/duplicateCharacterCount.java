6f009090930468db8b582208519326f5988d1216
kishan maurya <kkmaurya.0095@gmail.com>
2025-11-23 13:23:02 +0530

    duplicate chaar in string

--- src/duplicateCharacterCount.java
+++ src/duplicateCharacterCount.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+import java.util.HashMap;
+import java.util.Set;
 
 public class duplicateCharacterCount {
+    public static void main(String[] args) {
+        String string = "Learn Java Programming";
+        System.out.println(duplicateCharacterCounts(string));
+    }
+
+    public static HashMap<Character, Integer> duplicateCharacterCounts(String string) {
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < string.length(); i++) {
+            char ch = string.charAt(i);
+            if (ch != ' ') {   // ignore spaces (optional)
+                map.put(ch, map.getOrDefault(ch, 0) + 1);
+            }
+        }
+
+//        for (char ch : map.keySet()) {
+//            if (map.get(ch) > 1) {
+//                System.out.println(ch+ " , " +map.get(ch));
+//            }
+//        }
+        return map;
+    }
 }
